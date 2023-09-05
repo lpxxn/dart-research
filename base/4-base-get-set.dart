aaf4b0b7527e576ea5667234fae8d80d7c7fbc90
Aiden <mi_duo@live.com>
2023-09-05 20:33:56 +0800

    feat: test

--- base/4-base-get-set.dart
+++ base/4-base-get-set.dart
@@ -2,7 +2,8 @@ class ShoppingCart {
   List<double> _prices = [];
 
   // Add a "total" getter here:
-  double get totle => _prices.fold(0, (previousValue, element) => previousValue + element)
+  double get totle =>
+      _prices.fold(0, (previousValue, element) => previousValue + element);
 
   // Add a "prices" setter here:
   set price(List<double> value) {
